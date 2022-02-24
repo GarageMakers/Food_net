f0fb1a251b3b7f3ac287585bba10c9c0f26c77d0
dunkanhigh <oleg.savinov.2002@bk.ru>
2022-02-24 09:04:10 +0300

    start do models

---
+++ foodnet/foodnet/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30, help_text="Имя Фамилия")
+    eMail = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=20)
+    author = models.CharField(max_length=30)
+    preview = models.ImageField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class CommentList(models.Model):
+    recipe_name = models.CharField(max_length=20)
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=30)
+    text_field = models.TextField()
+
+
+# class Photo(models.Model):
+#     path = models.CharField(max_length=40)
+
+
+class Step(models.Model):
+    text_field = models.TextField()
+    photo = models.ImageField()
+
+
+class Ingredient(models.Model):
+    count = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.ingredient_name
