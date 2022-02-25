8f8a23166d10bffbea7427bc57ca0fc3e9e9c6e6
dunkanhigh <oleg.savinov.2002@bk.ru>
2022-02-25 18:32:43 +0300

    add env

--- foodnet/catalog/models.py
+++ foodnet/catalog/models.py
@@ -1,43 +1,43 @@
 from django.db import models
 
 
-# class User(models.Model):
-#     name = models.CharField(max_length=30, help_text="Имя Фамилия")
-#     eMail = models.EmailField()
-#     user_id = models.ForeignKey()
+class User(models.Model):
+    name = models.CharField(max_length=30, help_text="Имя Фамилия")
+    eMail = models.EmailField()
+    user_id = models.ForeignKey()
 
-#     def __str__(self):
-#         return self.name
-
-
-# class Recipe(models.Model):
-#     name = models.CharField(max_length=20)
-#     author = models.CharField(max_length=30)
-#     preview = models.ImageField()
-#     date = models.DateTimeField()
-
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 
-# class CommentList(models.Model):
-#     recipe_name = models.CharField(max_length=20)
+class Recipe(models.Model):
+    name = models.CharField(max_length=20)
+    author = models.CharField(max_length=30)
+    preview = models.ImageField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
 
 
-# class Comment(models.Model):
-#     author = models.CharField(max_length=30)
-#     text_field = models.TextField()
+class CommentList(models.Model):
+    recipe_name = models.CharField(max_length=20)
 
 
-# class Step(models.Model):
-#     text_field = models.TextField()
-#     photo = models.ImageField()
+class Comment(models.Model):
+    author = models.CharField(max_length=30)
+    text_field = models.TextField()
 
 
-# class Ingredient(models.Model):
-#     count = models.CharField(max_length=20)
-#     type = models.CharField(max_length=20)
-#     name = models.CharField(max_length=20)
+class Step(models.Model):
+    text_field = models.TextField()
+    photo = models.ImageField()
 
-#     def __str__(self):
-#         return self.name
+
+class Ingredient(models.Model):
+    count = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
