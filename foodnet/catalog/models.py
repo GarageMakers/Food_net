ee93317b91e686f14c70209901f0e7276f9db424
dunkanhigh <oleg.savinov.2002@bk.ru>
2022-03-18 10:46:51 +0300

    do models

--- foodnet/catalog/models.py
+++ foodnet/catalog/models.py
@@ -1,16 +1,24 @@
+from email.policy import default
 from django.db import models
 
 
 class User(models.Model):
+
+    user_id = models.AutoField()
+
     name = models.CharField(max_length=30, help_text="Имя Фамилия")
     eMail = models.EmailField()
-    user_id = models.ForeignKey()
+    isBanned = models.BooleanField(default=False)
+    password = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
 
 
 class Recipe(models.Model):
+    author_id = models.AutoField()
+    user_id = models.ForeignKey()
+
     name = models.CharField(max_length=20)
     author = models.CharField(max_length=30)
     preview = models.ImageField()
@@ -20,24 +28,36 @@ class Recipe(models.Model):
         return self.name
 
 
-class CommentList(models.Model):
-    recipe_name = models.CharField(max_length=20)
-
-
 class Comment(models.Model):
+    recept_id = models.ForeignKey()
+
     author = models.CharField(max_length=30)
     text_field = models.TextField()
 
 
 class Step(models.Model):
+
+    step_id = models.AutoField()
+    recept_id = models.ForeignKey()
+
     text_field = models.TextField()
     photo = models.ImageField()
 
 
 class Ingredient(models.Model):
+
+    igr_id = models.AutoField()
+    recept_id = models.ForeignKey()
+
     count = models.CharField(max_length=20)
     type = models.CharField(max_length=20)
     name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
+
+class Photo(models.Model):
+    photo_id = models.AutoField()
+    step_id = models.ForeignKey()
+
+    path = models.ImageField()
