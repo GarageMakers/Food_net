0664f3dbaf07bfd00213786223ebc1531b5a76c6
dunkanhigh <oleg.savinov.2002@bk.ru>
2022-03-20 14:09:51 +0300

    some adds

--- foodnet/catalog/models.py
+++ foodnet/catalog/models.py
@@ -9,6 +9,7 @@ class User(models.Model):
     eMail = models.EmailField()
     isBanned = models.BooleanField(default=False)
     password = models.CharField(max_length=20)
+    reg_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
