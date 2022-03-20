b70d3af10966d7921e984649a68053461cb0672d
dunkanhigh <oleg.savinov.2002@bk.ru>
2022-03-20 18:52:23 +0300

    some fix models and reg them in admin

--- foodnet/catalog/models.py
+++ foodnet/catalog/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
     eMail = models.EmailField()
     isBanned = models.BooleanField(default=False)
     password = models.CharField(max_length=20)
-    reg_date = models.DateTimeField(auto_now_add=True)
+    reg_date = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
