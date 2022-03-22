48d010e22e0e553983716242630d85475aee52ec
dunkanhigh <oleg.savinov.2002@bk.ru>
2022-03-22 11:58:02 +0300

    add requests

--- foodnet/catalog/views.py
+++ foodnet/catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(response):
+    return HttpResponse("<h1>index<h1>")
+
+
+def recipe(response):
+    return HttpResponse("<h1>recipe<h1>")
+
+
+def account(response):
+    return HttpResponse("<h1>account<h1>")
