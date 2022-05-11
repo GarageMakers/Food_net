604a6f349c478aeef3570626615b901f338a3aeb
dunkanhigh <oleg.savinov.2002@bk.ru>
2022-05-11 09:21:34 +0300

    improve authourization

--- foodnet/catalog/models.py
+++ foodnet/catalog/models.py
@@ -15,9 +15,9 @@ class Visitor(models.Model):
         if created:
             Visitor.objects.create(user=instance, isBanned=False)
 
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
+    # @receiver(post_save, sender=User)
+    # def save_user_profile(sender, instance, **kwargs):
+    #     instance.visitor.save()
 
     def __str__(self):
         return self.user.get_full_name()
