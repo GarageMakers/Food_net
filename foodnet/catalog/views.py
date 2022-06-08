b6c721ce7315dfb4e9447c3f066e5d3170aa7200
dunkanhigh <oleg.savinov.2002@bk.ru>
2022-06-08 20:03:43 +0300

    some changes

--- foodnet/catalog/views.py
+++ foodnet/catalog/views.py
@@ -4,19 +4,42 @@ from django.contrib.auth.views import LoginView
 from django.http import Http404, HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, TemplateView, DetailView
+from django.views.generic import CreateView, ListView, UpdateView, DetailView
 
 from .forms import *
 from .models import Recipe
-from .utils import DataMixin
+from .utils import DataMixin, FormsetMixin
 
 
 def top(response):
     return HttpResponse("<h1>silence<h1>")
 
 
-def silence(response):
-    return HttpResponse("<h1>silence<h1>")
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = "register.html"
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items())+list(c_def.items()))
+
+    # def get_success_url(self) -> str:
+    #     return reverse_lazy('login')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items())+list(c_def.items()))
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('index')
 
 
 class IndexView(DataMixin, ListView):
@@ -72,95 +95,6 @@ class VisitorRecipesView(DataMixin, ListView):
         return self.render_to_response(context)
 
 
-class DisplayRecipe(DataMixin, TemplateView):
-    model = Recipe
-    template_name = ""
-    context_object_name = "recipe"
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title="Мои рецепты")
-        return dict(list(context.items())+list(c_def.items()))
-
-
-class RegisterUser(DataMixin, CreateView):
-    form_class = RegisterUserForm
-    template_name = "register.html"
-    success_url = reverse_lazy('login')
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title="Регистрация")
-        return dict(list(context.items())+list(c_def.items()))
-
-    # def get_success_url(self) -> str:
-    #     return reverse_lazy('login')
-
-
-class LoginUser(DataMixin, LoginView):
-    form_class = LoginUserForm
-    template_name = 'login.html'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title="Авторизация")
-        return dict(list(context.items())+list(c_def.items()))
-
-    def get_success_url(self) -> str:
-        return reverse_lazy('index')
-
-
-class FormsetMixin():
-    object = None
-
-    def get(self, request, *args, **kwargs):
-        if getattr(self, 'is_update_view', False):
-            self.object = self.get_object()
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        formset_class = self.get_formset_class()
-        formset = self.get_formset(formset_class)
-        return self.render_to_response(self.get_context_data(form=form, formset=formset))
-
-    def post(self, request, *args, **kwargs):
-        if getattr(self, 'is_update_view', False):
-            self.object = self.get_object()
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        formset_class = self.get_formset_class()
-        formset = self.get_formset(formset_class)
-        if form.is_valid() and formset.is_valid():
-            return self.form_valid(form, formset)
-        else:
-            return self.form_invalid(form, formset)
-
-    def get_formset_class(self):
-        return self.formset_class
-
-    def get_formset(self, formset_class):
-        return formset_class(**self.get_formset_kwargs())
-
-    def get_formset_kwargs(self):
-        kwargs = {
-            'instance': self.object
-        }
-        if self.request.method in ('POST', 'PUT'):
-            kwargs.update({
-                'data': self.request.POST,
-                'files': self.request.FILES,
-            })
-        return kwargs
-
-    def form_valid(self, form, formset):
-        self.object = form.save()
-        formset.instance = self.object
-        formset.save()
-        return redirect(self.object.get_absolute_url())
-
-    def form_invalid(self, form, formset):
-        return self.render_to_response(self.get_context_data(form=form, formset=formset))
-
-
 class AddRecipe(FormsetMixin, CreateView, DataMixin):
     model = Recipe
     template_name = 'addRecipe.html'
@@ -178,3 +112,16 @@ class AddRecipe(FormsetMixin, CreateView, DataMixin):
         formset.instance = self.object
         formset.save()
         return redirect(self.object.get_absolute_url())
+
+
+class UpdateRecipeForm(FormsetMixin, DataMixin, UpdateView):
+    model = Recipe
+    template_name = 'updateRecipe.html'
+    form_class = AddRecipeForm
+    formset_class = RecipeStepFormSet
+    template_name_suffix = '_update_form'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Редактирование")
+        return dict(list(context.items())+list(c_def.items()))
