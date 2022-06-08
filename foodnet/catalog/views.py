8299ae3dc39b375e582c1863bd7f2b4a28c98386
dunkanhigh <oleg.savinov.2002@bk.ru>
2022-06-08 20:14:52 +0300

    clear code

--- foodnet/catalog/views.py
+++ foodnet/catalog/views.py
@@ -62,17 +62,6 @@ class VisitorRecipesView(DataMixin, ListView):
         return self.render_to_response(context)
 
 
-class DisplayRecipe(DataMixin, TemplateView):
-    model = Recipe
-    template_name = ""
-    context_object_name = "recipe"
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title="Мои рецепты")
-        return dict(list(context.items())+list(c_def.items()))
-
-
 class RegisterUser(DataMixin, CreateView):
     form_class = RegisterUserForm
     template_name = "register.html"
